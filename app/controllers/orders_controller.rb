277bf6e2301d7bbac1f3c931ce64193ca7125dbc
James Lotruglio <jimlotruglio@yahoo.com>
2018-12-22 17:44:59 +0000

    Automatically backed up by Learn

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,10 +6,10 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json {render json: @order, status: 200}
-    end 
+    #respond_to do |format|
+    #  format.html { render :show }
+    #  format.json {render json: @order, status: 200}
+    end
 
   end
 
